72ca906962af09c178f6d7f0470757c6fd8f0ae2
MH_LongVQ <longvq@mor.com.vn>
2022-07-19 15:37:43 +0700

    fix bug design

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  slidesPerView: 1,
+  slidesToShow: 1,
+  mousewheel: true,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 500,
+});
